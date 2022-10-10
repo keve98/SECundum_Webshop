e0c6501fb5d6adc12712e96dc526f598e4c4a7db
Tóth Keve András <toth.keve2@gmail.com>
2022-10-10 21:08:38 +0200

    init

---
+++ SECundum_WebshopServer/src/main/java/com/example/SECundum_WebshopServer/SeCundumWebshopServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SECundum_WebshopServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeCundumWebshopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeCundumWebshopServerApplication.class, args);
+	}
+
+}
---
+++ SECundum_WebshopServer/src/test/java/com/example/SECundum_WebshopServer/SeCundumWebshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SECundum_WebshopServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeCundumWebshopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
