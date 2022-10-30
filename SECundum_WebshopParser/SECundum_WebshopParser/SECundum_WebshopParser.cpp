9248b6e420f9f1665b4742b9b91985200e39518d
KocsiLevente <kocsilevi@gmail.com>
2022-10-30 10:25:53 +0100

    Added gitignore.

---
+++ SECundum_WebshopParser/SECundum_WebshopParser/SECundum_WebshopParser.cpp
@@ -0,0 +1,12 @@
+﻿// SECundum_WebshopParser.cpp : Defines the entry point for the application.
+//
+
+#include "SECundum_WebshopParser.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
