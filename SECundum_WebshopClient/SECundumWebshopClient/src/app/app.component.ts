2e34a50a5038680897d3a376ea37b24bb9d3104f
bazsarmartin <bazsarmartin1@gmail.com>
2022-12-01 20:03:58 +0100

    Added image visualization

--- SECundum_WebshopClient/SECundumWebshopClient/src/app/app.component.ts
+++ SECundum_WebshopClient/SECundumWebshopClient/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SECundumWebshopClient';
+
+  private _albums: any = [];
+  constructor(private _lightbox: Lightbox) {
+    for (let i = 1; i <= 3; i++) {
+      const src = './assets/caff_pictures/image' + i + '.png';
+      const caption = 'Image ' + i + 'caption';
+      const thumb = './assets/caff_pictures/image' + i + '.png';
+      const album = { src: src, caption: caption, thumb: thumb};
+      this._albums.push(album);
+    }
+  }
+
+  open(index: number): void {
+    this._lightbox.open(this._albums, index);
+  }
+
+  close(): void {
+    this._lightbox.close();
+  }
 }
