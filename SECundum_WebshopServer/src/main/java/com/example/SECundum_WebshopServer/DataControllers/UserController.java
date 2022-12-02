efa5d2153e1b5085b0fbd0664dacbb4c86e24f96
Tóth Keve András <toth.keve2@gmail.com>
2022-12-02 22:53:27 +0100

    cross origin fix

--- SECundum_WebshopServer/src/main/java/com/example/SECundum_WebshopServer/DataControllers/UserController.java
+++ SECundum_WebshopServer/src/main/java/com/example/SECundum_WebshopServer/DataControllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.ExecutionException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     public UserService userService;
