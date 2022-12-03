0ccae89c10a7d733c3dbd43d94eb81f685b7a776
bazsarmartin <bazsarmartin1@gmail.com>
2022-12-03 18:42:48 +0100

    Fixed comments

--- SECundum_WebshopClient/SECundumWebshopClient/src/app/photo-details/photo-details.component.ts
+++ SECundum_WebshopClient/SECundumWebshopClient/src/app/photo-details/photo-details.component.ts
@@ -13,21 +13,22 @@ export class PhotoDetailsComponent implements OnInit {
   public imgName: string=sessionStorage.getItem('clickedImageName').replace('_', " ");
   public imgPath: string='./assets/caff_pictures/'+sessionStorage.getItem('clickedImageName')+'.png';
   public comments: Comment[] = [];
+  private _albums: any = [];
 
   constructor(private commentService: CommentService) {
-    this.commentService.getAllComment(this.imgName).subscribe(data => {
+    this.commentService.getAllComment(sessionStorage.getItem('clickedImageName')).subscribe(data => {
       this.comments=data;
       this.showComments();
     })
   }
 
-   showComments():void{
+  showComments():void{
     for(const element of this.comments){
       const id = element.id;
       const caff_name = element.caff_name;
       const comment = element.comment;
       const comm = { id: id, caff_name: caff_name, comment: comment};
-      this.comments.push(comm);
+      this._albums.push(comm);
     }
   }
 
